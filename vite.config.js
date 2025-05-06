318a1950a8eb0368a6c1ea204e04f8f86d02cfda
paytonwu90 <sw9008k@gmail.com>
2025-05-07 07:47:45 +0800

    安裝gh-pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/software-engineer-camping-2025/',
   root: resolve(__dirname, 'src'),
   build: {
     outDir: '../dist'
