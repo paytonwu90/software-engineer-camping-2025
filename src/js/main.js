cd0f8522a57e74ad2763aa49d89878abf0bb410c
paytonwu90 <sw9008k@gmail.com>
2025-05-07 07:40:30 +0800

    部落格大部分

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,10 @@ import '../scss/all.scss'
 
 // Import all of Bootstrap's JS
 //import * as bootstrap from 'bootstrap'
+
+
+document.querySelectorAll('a[href="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+  });
+});
