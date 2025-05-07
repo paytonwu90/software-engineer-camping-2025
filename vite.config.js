1bfc1f3dfe766bae090ae4bf50f6eee5d14558cb
paytonwu90 <sw9008k@gmail.com>
2025-05-08 06:32:05 +0800

    1. file rename 2. 調整 href 路徑 3. 調整 vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,21 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   base: '/software-engineer-camping-2025/',
   root: resolve(__dirname, 'src'),
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      '@images': resolve(__dirname, 'src/assets/images')
+    }
+  },
   build: {
-    outDir: '../dist'
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        blog: resolve(__dirname, 'src/blog.html')
+      }
+    }
   },
   server: {
     port: 8080
