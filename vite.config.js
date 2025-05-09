f97d9860e885d1539a3360789c44c9beff219c93
paytonwu90 <sw9008k@gmail.com>
2025-05-09 22:02:41 +0800

    vite config adjustment for Boostrap

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/software-engineer-camping-2025/',
+  base: '/software-engineer-camping-2025/', //加了 base 之後在 dev 網址會變成 http://localhost:8080/software-engineer-camping-2025/
+  //__dirname 是目前這個設定檔（vite.config.js）所在的資料夾
+  //resolve(__dirname, 'src') 會組合成完整路徑，例如 /你的專案路徑/src
+  //
   root: resolve(__dirname, 'src'),
   resolve: {
     alias: {
@@ -22,5 +25,18 @@ export default defineConfig({
   },
   server: {
     port: 8080
-  }
+  },
+  // Optional: Silence Sass deprecation warnings.
+  css: {
+    preprocessorOptions: {
+       scss: {
+         silenceDeprecations: [
+           'import',
+           'mixed-decls',
+           'color-functions',
+           'global-builtin',
+         ],
+       },
+    },
+ },
 })
